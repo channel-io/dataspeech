0ce5a3e8eeb7e8b02c1540150759aebd0abee10f
SeungyounShin <ssy10011218@gmail.com>
2024-12-26 04:44:29 +0000

    Add (Custom Korean Dataset Support) .

--- scripts/run_prompt_creation_vllm.py
+++ scripts/run_prompt_creation_vllm.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import re
 from datasets import DatasetDict, load_dataset
 from vllm import LLM, SamplingParams
 
@@ -10,13 +11,31 @@ SELECTED_KEYS_MEANING = {
 }
 
 SYSTEM_PROMPT = """
-Given a speaking attributes of a speech, create a description of the speech. 
-You should answer in the following format:
+You are tasked with generating a concise and accurate description of a speech sample based on provided speaking attributes. 
+
++ Use only the information provided in the attributes to construct the description.
++ Ensure that all provided attributes are included in the description.
++ The description should be grammatically correct, easy to understand, and concise.
++ Do not infer or include any additional details that are not explicitly stated in the attributes.
+
+Format your response as follows:
 
 <description>
 {description}
 </description>
+
+### Example:
+Given the speaking attributes:
+- speaking loudness: speaking quietly
+- speaking speed: moderately
+- metadata: 30대 여자 목소리
+
+### Output:
+<description>
+A soft-spoken woman in her 30s with a moderate speaking pace.
+</description>
 """
+
 
 def create_description(batch, args, llm):
     descriptions = []
@@ -41,6 +60,7 @@ def create_description(batch, args, llm):
     templated_convs = tokenizer.apply_chat_template(
         convs,
         tokenize=False,
+        add_generation_prompt=True,
     )
 
     completions = [[] for _ in range(len(descriptions))]
@@ -59,10 +79,16 @@ def create_description(batch, args, llm):
         use_tqdm=False,
     )
 
-    print(convs[0])
-    print(responses[0])
-    print("===============")
-    # batch["description"] = descriptions
+    output_texts = [responses[idx].outputs[0].text for idx in range(len(responses))]
+    output_tokens = [responses[idx].outputs[0].token_ids for idx in range(len(responses))]
+
+    # only allow alphabets, numbers, spaces, and some special characters
+    output_texts_refined = [
+        ' '.join(re.findall(r"[a-zA-Z0-9\s.,'\"-]+", text.replace('<description>\n', '').replace('\n</description>', '').strip()))
+        for text in output_texts
+    ]
+    
+    batch["description"] = output_texts_refined
     return batch
 
 def run(args):
@@ -90,11 +116,18 @@ def run(args):
         load_from_cache_file=False,
     )
 
+    if args.hf_dataset_name_output:
+        dataset.push_to_hub(args.hf_dataset_name_output, private=True)
+        print(f"Pushed to hub: {args.hf_dataset_name_output}")
+    else:
+        print("Not pushing to hub")
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--hf_dataset_name", type=str, required=True)
-    parser.add_argument("--model_name", type=str, default="beomi/EXAONE-3.5-7.8B-Instruct-Llamafied")
+    parser.add_argument("--hf_dataset_name_output", type=str, required=False)
+    parser.add_argument("--model_name", type=str, default="meta-llama/Llama-3.2-3B-Instruct")
     parser.add_argument("--gpu_memory_utilization", type=float, default=0.7)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--tensor_parallel_size", type=int, default=torch.cuda.device_count())
